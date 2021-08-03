0550a140f2a3b245512a307d590e9f2b5087acaa
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-08-03 15:48:47 +0100

    added tests

--- backend/handlers.js
+++ backend/handlers.js
@@ -1,14 +1,14 @@
 const websites = require('./websites');
 
 function handleQuery(query) {
-    var res = websites.filter( website => {
+    let res = websites.filter( website => {
             return (website.includes(query));
         });
     return res;
 }
 
 function handleFetchByID(id) {
-    // return id > websites.length - 1 ? websites[id] : websites[websites.length - 1];
+    return id < websites.length - 1 ? { [websites[id]] : id } : { [websites[websites.length-1]] : websites.length };
     return { [websites[id]] : id };
 }
 
--- backend/test/handlers.test.js
+++ backend/test/handlers.test.js
@@ -1,10 +1,21 @@
 const { handleQuery, handleFetchByID } = require('../handlers');
+const websites = require('../websites');
 
 describe('handlers', () => {
     test('handleQuery should return website url that contains query', () => {
-        expect( handleQuery('2') ).toEqual(["test2.com"]);
+        expect(handleQuery('2')).toEqual(["test2.com"]);
     })
+
+    test('handleQuery should return an empty array if no urls match query', () => {
+        expect(handleQuery('sdfjkasdghjsdfbnmxcvhjkasdgjh')).toEqual([]);
+    })
+
     test('handleFetchByID should return website url at index of ID', () => {
-        expect( handleFetchByID('2') ).toEqual({"test2.com":"1"});
+        expect(handleFetchByID('2')).toEqual({"test3.com": 3});
     })
+
+    test('handleFetchByID should return last website in array if index is out of range', () => {
+        expect(handleFetchByID('999999')).toEqual({"test3.com": 3});
+    })
+
 })
