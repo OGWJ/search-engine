1122be72b370f64d27f0ea17722574079ed9681a
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-08-04 15:56:23 +0100

    fixed problem with merge

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const websites = require('./websites');
 const { handleQuery, handleFetchByID } = require('./handlers');
 const { spellCheck, getRandIdx } = require('./utils');
 
